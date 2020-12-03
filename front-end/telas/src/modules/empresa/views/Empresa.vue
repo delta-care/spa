cb1375eed2ab3155d1b4db0000185622a49df3e0
David Carvalho <jdavidsilv@gmail.com>
2020-12-02 21:47:48 -0300

    add campos em empresa

--- front-end/telas/src/modules/empresa/views/Empresa.vue
+++ front-end/telas/src/modules/empresa/views/Empresa.vue
@@ -13,27 +13,30 @@
           </v-col>
           <v-col cols="12" sm="2">
             <v-menu
-              ref="menu1"
-              v-model="menu1"
+              ref="menuDataAdmissao1"
+              v-model="menuDataAdmissao1"
               :close-on-content-click="false"
               transition="scale-transition"
               offset-y
+              max-width="290px"
+              min-width="290px"
             >
               <template v-slot:activator="{ on, attrs }">
                 <v-text-field
+                  v-model="dataAdmissaoFormatada1"
                   label="Data Admissão"
                   persistent-hint
                   v-bind="attrs"
-                  @blur="date = parseDate(dateFormatted)"
+                  @blur="dataAdmissao1 = parseDate(dataAdmissaoFormatada1)"
                   v-on="on"
                   outlined
                   disabled
                 ></v-text-field>
               </template>
               <v-date-picker
-                v-model="date"
+                v-model="dataAdmissao1"
                 no-title
-                @input="menu1 = false"
+                @input="menuDataAdmissao1 = false"
               ></v-date-picker>
             </v-menu>
           </v-col>
@@ -43,14 +46,325 @@
         </v-row>
         <v-card>
           <v-tabs v-model="tab" background-color="primary" dark>
-            <v-tab v-for="item in items" :key="item.tab">
-              {{ item.tab }}
-            </v-tab>
+            <v-tab key="Dados Básicos"> Dados Básicos </v-tab>
+            <v-tab key="Produtos e Coberturas"> Produtos e Coberturas </v-tab>
           </v-tabs>
+
           <v-tabs-items v-model="tab">
-            <v-tab-item v-for="item in items" :key="item.tab">
+            <v-tab-item key="Dados Básicos">
               <v-card>
-                <v-card-text>{{ item.content }}</v-card-text>
+                <v-card-text>
+                  <v-row>
+                    <v-col cols="12" sm="2">
+                      <v-text-field
+                        label="Código"
+                        outlined
+                        hide-details="auto"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="4">
+                      <v-text-field
+                        label="CNPJ"
+                        outlined
+                        hide-details="auto"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="2">
+                      <v-menu
+                        ref="menuDataAdmissao2"
+                        v-model="menuDataAdmissao2"
+                        :close-on-content-click="false"
+                        transition="scale-transition"
+                        offset-y
+                        max-width="290px"
+                        min-width="290px"
+                      >
+                        <template v-slot:activator="{ on, attrs }">
+                          <v-text-field
+                            v-model="dataAdmissaoFormatada2"
+                            label="Data Admissão"
+                            persistent-hint
+                            v-bind="attrs"
+                            @blur="
+                              dataAdmissao2 = parseDate(dataAdmissaoFormatada2)
+                            "
+                            v-on="on"
+                            outlined
+                            hide-details="auto"
+                          ></v-text-field>
+                        </template>
+                        <v-date-picker
+                          v-model="dataAdmissao2"
+                          no-title
+                          @input="menuDataAdmissao2 = false"
+                        ></v-date-picker>
+                      </v-menu>
+                    </v-col>
+                    <v-col cols="12" sm="2">
+                      <v-combobox
+                        outlined
+                        label="Filial Cont."
+                        hide-details="auto"
+                      ></v-combobox>
+                    </v-col>
+                    <v-col cols="12" sm="2">
+                      <v-combobox
+                        outlined
+                        label="Administ."
+                        hide-details="auto"
+                      ></v-combobox>
+                    </v-col>
+                  </v-row>
+                  <v-row>
+                    <v-col cols="12" sm="4">
+                      <v-text-field
+                        label="Nome"
+                        outlined
+                        hide-details="auto"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="2">
+                      <v-combobox
+                        outlined
+                        label="UF"
+                        hide-details="auto"
+                      ></v-combobox>
+                    </v-col>
+                    <v-col cols="12" sm="3">
+                      <v-text-field
+                        label="C.E.I"
+                        outlined
+                        hide-details="auto"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="3">
+                      <v-text-field
+                        label="Inscrição Municipal"
+                        outlined
+                        hide-details="auto"
+                      ></v-text-field>
+                    </v-col>
+                  </v-row>
+                  <v-row>
+                    <v-col cols="12" sm="2">
+                      <v-text-field
+                        label="CEP"
+                        outlined
+                        hide-details="auto"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="4">
+                      <v-text-field
+                        label="Logradouro"
+                        outlined
+                        hide-details="auto"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="3">
+                      <v-text-field
+                        label="Bairro"
+                        outlined
+                        hide-details="auto"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="3">
+                      <v-text-field
+                        label="Email"
+                        outlined
+                        hide-details="auto"
+                      ></v-text-field>
+                    </v-col>
+                  </v-row>
+                </v-card-text>
+                <v-divider></v-divider>
+                <v-card-actions>
+                  <v-row class="mt-2 ml-2 pb-2">
+                    <v-btn color="primary" large>Salvar</v-btn>
+                  </v-row>
+                </v-card-actions>
+              </v-card>
+            </v-tab-item>
+            <v-tab-item key="Produtos e Coberturas">
+              <v-card>
+                <v-card-text>
+                  <h2>Produto</h2>
+                  <v-divider class="mt-2"></v-divider>
+                  <v-row class="mt-2">
+                    <v-col cols="12" sm="3">
+                      <v-combobox
+                        outlined
+                        label="Plano"
+                        hide-details="auto"
+                      ></v-combobox>
+                    </v-col>
+                    <v-col cols="12" sm="3">
+                      <v-combobox
+                        outlined
+                        label="Plano"
+                        hide-details="auto"
+                      ></v-combobox>
+                    </v-col>
+                    <v-col cols="12" sm="2">
+                      <v-combobox
+                        outlined
+                        label="Versão"
+                        hide-details="auto"
+                      ></v-combobox>
+                    </v-col>
+                    <v-col cols="12" sm="2">
+                      <v-menu
+                        ref="dataVigenciaInicioMenu"
+                        v-model="dataVigenciaInicioMenu"
+                        :close-on-content-click="false"
+                        transition="scale-transition"
+                        offset-y
+                        max-width="290px"
+                        min-width="290px"
+                      >
+                        <template v-slot:activator="{ on, attrs }">
+                          <v-text-field
+                            v-model="dataVigenciaInicioFormatada"
+                            label="Início de Vigência"
+                            persistent-hint
+                            v-bind="attrs"
+                            @blur="
+                              dataVigenciaInicio = parseDate(
+                                dataVigenciaInicioFormatada
+                              )
+                            "
+                            v-on="on"
+                            outlined
+                          ></v-text-field>
+                        </template>
+                        <v-date-picker
+                          v-model="dataVigenciaInicio"
+                          no-title
+                          @input="dataVigenciaInicioMenu = false"
+                        ></v-date-picker>
+                      </v-menu>
+                    </v-col>
+                    <v-col cols="12" sm="2">
+                      <v-menu
+                        ref="dataVigenciaFimMenu"
+                        v-model="dataVigenciaFimMenu"
+                        :close-on-content-click="false"
+                        transition="scale-transition"
+                        offset-y
+                        max-width="290px"
+                        min-width="290px"
+                      >
+                        <template v-slot:activator="{ on, attrs }">
+                          <v-text-field
+                            v-model="dataVigenciaFimFormatada"
+                            label="Fim de Vigência"
+                            persistent-hint
+                            v-bind="attrs"
+                            @blur="
+                              dataVigenciaFim = parseDate(
+                                dataVigenciaFimFormatada
+                              )
+                            "
+                            v-on="on"
+                            outlined
+                          ></v-text-field>
+                        </template>
+                        <v-date-picker
+                          v-model="dataVigenciaFim"
+                          no-title
+                          @input="dataVigenciaFimMenu = false"
+                        ></v-date-picker>
+                      </v-menu>
+                    </v-col>
+                  </v-row>
+
+                  <h2>Coberturas</h2>
+                  <v-divider class="mt-2"></v-divider>
+                  <v-row class="mt-2">
+                    <v-col cols="12" sm="6">
+                      <v-combobox
+                        outlined
+                        label="Benefício"
+                        hide-details="auto"
+                      ></v-combobox>
+                    </v-col>
+                    <v-col cols="12" sm="2">
+                      <v-menu
+                        ref="dataVigenciaInicioCoberturaMenu"
+                        v-model="dataVigenciaInicioCoberturaMenu"
+                        :close-on-content-click="false"
+                        transition="scale-transition"
+                        offset-y
+                        max-width="290px"
+                        min-width="290px"
+                      >
+                        <template v-slot:activator="{ on, attrs }">
+                          <v-text-field
+                            v-model="dataVigenciaInicioCoberturaFormatada"
+                            label="Início de Vigência"
+                            persistent-hint
+                            v-bind="attrs"
+                            @blur="
+                              dataVigenciaInicioCobertura = parseDate(
+                                dataVigenciaInicioCoberturaFormatada
+                              )
+                            "
+                            v-on="on"
+                            outlined
+                          ></v-text-field>
+                        </template>
+                        <v-date-picker
+                          v-model="dataVigenciaInicioCobertura"
+                          no-title
+                          @input="dataVigenciaInicioCoberturaMenu = false"
+                        ></v-date-picker>
+                      </v-menu>
+                    </v-col>
+                    <v-col cols="12" sm="2">
+                      <v-menu
+                        ref="dataVigenciaFimCoberturaMenu"
+                        v-model="dataVigenciaFimCoberturaMenu"
+                        :close-on-content-click="false"
+                        transition="scale-transition"
+                        offset-y
+                        max-width="290px"
+                        min-width="290px"
+                      >
+                        <template v-slot:activator="{ on, attrs }">
+                          <v-text-field
+                            v-model="dataVigenciaFimCoberturaFormatada"
+                            label="Fim de Vigência"
+                            persistent-hint
+                            v-bind="attrs"
+                            @blur="
+                              dataVigenciaFimCobertura = parseDate(
+                                dataVigenciaFimCoberturaFormatada
+                              )
+                            "
+                            v-on="on"
+                            outlined
+                          ></v-text-field>
+                        </template>
+                        <v-date-picker
+                          v-model="dataVigenciaFimCobertura"
+                          no-title
+                          @input="dataVigenciaFimCoberturaMenu = false"
+                        ></v-date-picker>
+                      </v-menu>
+                    </v-col>
+                    <v-col cols="12" sm="2">
+                      <v-text-field
+                        label="Valor"
+                        outlined
+                      ></v-text-field>
+                    </v-col>
+                  </v-row>
+                </v-card-text>
+                <v-divider></v-divider>
+                <v-card-actions>
+                  <v-row class="mt-2 ml-2 pb-2">
+                    <v-btn color="primary" large>Salvar</v-btn>
+                  </v-row>
+                </v-card-actions>
               </v-card>
             </v-tab-item>
           </v-tabs-items>
@@ -72,34 +386,77 @@ export default {
     AppMenu,
   },
 
-  data: (vm) => ({
-    date: new Date().toISOString().substr(0, 10),
-    dateFormatted: vm.formatDate(new Date().toISOString().substr(0, 10)),
-    menu1: false,
-    menu2: false,
+  data: () => ({
+    dataAdmissao1: "",
+    dataAdmissaoFormatada1: "",
+    menuDataAdmissao1: false,
+
+    dataAdmissao2: "",
+    dataAdmissaoFormatada2: "",
+    menuDataAdmissao2: false,
+
+    dataVigenciaInicio: "",
+    dataVigenciaInicioFormatada: "",
+    dataVigenciaInicioMenu: false,
+
+    dataVigenciaFim: "",
+    dataVigenciaFimFormatada: "",
+    dataVigenciaFimMenu: false,
+
+    dataVigenciaInicioCobertura: "",
+    dataVigenciaInicioCoberturaFormatada: "",
+    dataVigenciaInicioCoberturaMenu: false,
+
+    dataVigenciaFimCobertura: "",
+    dataVigenciaFimCoberturaFormatada: "",
+    dataVigenciaFimCoberturaMenu: false,
+
     tab: null,
-    items: [
-      { tab: "Dados Básicos", content: "Tab 1 Content" },
-      { tab: "Produtos e Coberturas", content: "Tab 2 Content" },
-      { tab: "Cláusulas Contratuais", content: "Tab 3 Content" },
-      { tab: "Complementos Cadastrais", content: "Tab 4 Content" },
-      { tab: "Taxa Adicional", content: "Tab 5 Content" },
-      { tab: "Ocorrência", content: "Tab 6 Content" },
-      { tab: "Regulação", content: "Tab 7 Content" },
-      { tab: "Liminar", content: "Tab 8 Content" },
-      { tab: "Regra de Suspensão", content: "Tab 9 Content" },
-    ],
   }),
 
   computed: {
-    computedDateFormatted() {
-      return this.formatDate(this.date);
+    computedDataAdmissaoFormatada() {
+      return this.formatDate(this.dataAdmissao1);
+    },
+    computedDataAdmissaoFormatada2() {
+      return this.formatDate(this.dataAdmissao2);
+    },
+    computedDataVigenciaInicioFormatada() {
+      return this.formatDate(this.dataVigenciaInicio);
+    },
+    computedDataVigenciaFimFormatada() {
+      return this.formatDate(this.dataVigenciaFim);
+    },
+      computedDataVigenciaInicioCoberturaFormatada() {
+      return this.formatDate(this.dataVigenciaInicioCobertura);
+    },
+    computedDataVigenciaFimCoberturaFormatada() {
+      return this.formatDate(this.dataVigenciaFimCobertura);
     },
   },
 
   watch: {
-    date() {
-      this.dateFormatted = this.formatDate(this.date);
+    dataAdmissao1() {
+      this.dataAdmissaoFormatada1 = this.formatDate(this.dataAdmissao1);
+    },
+    dataAdmissao2() {
+      this.dataAdmissaoFormatada2 = this.formatDate(this.dataAdmissao2);
+    },
+    dataVigenciaInicio() {
+      this.dataVigenciaInicioFormatada = this.formatDate(
+        this.dataVigenciaInicio
+      );
+    },
+    dataVigenciaFim() {
+      this.dataVigenciaFimFormatada = this.formatDate(this.dataVigenciaFim);
+    },
+    dataVigenciaInicioCobertura() {
+      this.dataVigenciaInicioCoberturaFormatada = this.formatDate(
+        this.dataVigenciaInicioCobertura
+      );
+    },
+    dataVigenciaFimCobertura() {
+      this.dataVigenciaFimCoberturaFormatada = this.formatDate(this.dataVigenciaFimCobertura);
     },
   },
 
