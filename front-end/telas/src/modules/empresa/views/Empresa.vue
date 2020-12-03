392ff05cf4db396a51298badc9d73cb75c0cb8ae
David Carvalho <jdavidsilv@gmail.com>
2020-12-03 14:36:08 -0300

    add edição empresa inicial

--- front-end/telas/src/modules/empresa/views/Empresa.vue
+++ front-end/telas/src/modules/empresa/views/Empresa.vue
@@ -143,33 +143,6 @@
                                         </v-card-actions>
                                     </v-card>
                                 </v-dialog>
-                                <v-dialog
-                                    v-model="dialogDelete"
-                                    max-width="500px"
-                                >
-                                    <v-card>
-                                        <v-card-title class="headline"
-                                            >Are you sure you want to delete
-                                            this item?</v-card-title
-                                        >
-                                        <v-card-actions>
-                                            <v-spacer></v-spacer>
-                                            <v-btn
-                                                color="blue darken-1"
-                                                text
-                                                @click="closeDelete"
-                                                >Cancel</v-btn
-                                            >
-                                            <v-btn
-                                                color="blue darken-1"
-                                                text
-                                                @click="deleteItemConfirm"
-                                                >OK</v-btn
-                                            >
-                                            <v-spacer></v-spacer>
-                                        </v-card-actions>
-                                    </v-card>
-                                </v-dialog>
                             </template>
                             <template v-slot:[`item.actions`]="{ item }">
                                 <v-icon class="ml-3" @click="editItem(item)">
@@ -258,6 +231,7 @@
                                                 label="Código"
                                                 outlined
                                                 hide-details="auto"
+                                                v-model="empresa.codigo"
                                             ></v-text-field>
                                         </v-col>
                                         <v-col cols="12" sm="4">
@@ -265,6 +239,7 @@
                                                 label="CNPJ"
                                                 outlined
                                                 hide-details="auto"
+                                                v-model="empresa.cnpj"
                                             ></v-text-field>
                                         </v-col>
                                         <v-col cols="12" sm="2">
@@ -330,6 +305,7 @@
                                                 label="Nome"
                                                 outlined
                                                 hide-details="auto"
+                                                v-model="empresa.nome"
                                             ></v-text-field>
                                         </v-col>
                                         <v-col cols="12" sm="2">
@@ -687,6 +663,13 @@ export default {
             { text: "Visualizar", value: "actions", sortable: false },
         ],
         empresas: [],
+        empresa: {
+            codigo: "",
+            cnpj: "",
+            nome: "",
+            produtos: "",
+            categorias: ""
+        },
         editedIndex: -1,
         editedItem: {
             name: "",
@@ -764,7 +747,7 @@ export default {
     },
 
     created() {
-        this.initialize();
+        this.pesquisar();
     },
 
     methods: {
@@ -871,20 +854,11 @@ export default {
         },
 
         editItem(item) {
-            this.editedIndex = this.empresas.indexOf(item);
-            this.editedItem = Object.assign({}, item);
-            this.dialog = true;
-        },
-
-        deleteItem(item) {
-            this.editedIndex = this.empresas.indexOf(item);
-            this.editedItem = Object.assign({}, item);
-            this.dialogDelete = true;
-        },
-
-        deleteItemConfirm() {
-            this.empresas.splice(this.editedIndex, 1);
-            this.closeDelete();
+            this.empresa = item;
+            //this.editedIndex = this.empresas.indexOf(item);
+            //console.log(item);
+            //this.editedItem = Object.assign({}, item);
+            //this.dialog = true;
         },
 
         close() {
