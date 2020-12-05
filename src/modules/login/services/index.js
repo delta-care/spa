dd24e610f73b2ef27d3c4784d798b9dc3907376f
David Carvalho <jdavidsilv@gmail.com>
2020-12-05 11:52:56 -0300

    alteração diretório

--- src/modules/login/services/index.js
+++ src/modules/login/services/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: process.env.VUE_APP_USUARIO_API,
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json'
