497baf8c73aaef2a3a13a0c1cc4e80dabed9984f
David Carvalho <jdavidsilv@gmail.com>
2020-12-18 00:59:19 -0300

    correção variáveis no sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 JSON_STRING='window.configs = { \
-  "VUE_APP_VARIABLE_1":"'"${VUE_APP_VARIABLE_1}"'", \
-  "VUE_APP_VARIABLE_2":"'"${VUE_APP_VARIABLE_2}"'" \
+  "VUE_APP_EMPRESA_API":"'"${VUE_APP_EMPRESA_API}"'", \
+  "VUE_APP_USUARIO_API":"'"${VUE_APP_USUARIO_API}"'" \
 }'
 
 sed -i "s@// CONFIGURATIONS_PLACEHOLDER@${JSON_STRING}@" /usr/share/nginx/html/index.html
