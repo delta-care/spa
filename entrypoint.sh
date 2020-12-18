223d4e805a8856252a0a9818e291dd963a8564a5
root <root@ip-172-31-27-154.us-east-2.compute.internal>
2020-12-18 04:12:38 +0000

    correção dir no entry*.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,6 @@ JSON_STRING='window.configs = { \
   "VUE_APP_USUARIO_API":"'"${VUE_APP_USUARIO_API}"'" \
 }'
 
-sed -i "s@// CONFIGURATIONS_PLACEHOLDER@${JSON_STRING}@" /usr/share/nginx/html/index.html
+sed -i "s@// CONFIGURATIONS_PLACEHOLDER@${JSON_STRING}@" /app/index.html
 
 exec "$@"
